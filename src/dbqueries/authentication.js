c7dcbb104fe3f21ef96643a2e099040c76927f9b
CabaVarga <caba.varga@gmail.com>
2019-12-01 21:15:55 +0100

    Authentication improved

--- src/dbqueries/authentication.js
+++ src/dbqueries/authentication.js
@@ -3,13 +3,19 @@ import FileSync from 'lowdb/adapters/FileSync'
 
 const atob = require('atob')
 
-
 const dbPath = 'src/db/'
 const dbFile = 'db.json'
 const dbFullPath = dbPath + dbFile
 
-const adapter = new FileSync(dbFullPath)
-const db = low(adapter)
+// const adapter = new FileSync(dbFullPath)
+// const db = low(adapter)
+
+const setupDb = () => {
+  // probably not a good idea -- new adapter every time, not sure about release..
+  const adapter = new FileSync(dbFullPath)
+  const db = low(adapter)
+  return db
+}
 
 const verifyCredentials = (credentials) => {
 
@@ -19,14 +25,25 @@ const verifyCredentials = (credentials) => {
 
   try {
     if (credentials.startsWith('Basic ')) {
+      console.log(credentials)
       const stripped = credentials.replace('Basic ', '')
+
+      if (stripped === 'null') {
+        console.log('Authorization header is Basic null.')
+        return {
+          error: 'Authorization header is empty.'
+        }
+      }
 
       const raw = atob(stripped)
 
       const details = raw.split(':')
 
-      const result = db.get('accounts').find({ username: details[0] }).value()
+      console.log('User: ', details[0], ' pass: ', details[1])
+      
+      const result = setupDb().get('accounts').find({ username: details[0] }).value()
 
+      console.log('RESULT: ', result)
       if (result) {
         if (result.password === details[1]) {
           return result
